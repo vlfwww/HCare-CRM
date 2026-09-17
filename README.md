d6a31efdb7b6a5360f1b569dada4372e4bc4f192
vlfwww <vlfwww@outlook.com>
2026-09-17 12:14:36 +0300

    feat: set up base structure

---
+++ README.md
@@ -0,0 +1,1 @@
+#HCare-CRM
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold bg-red-500 text-white p-4 rounded-lg">
+        HCare
+      </h1>
+    </div>
+  );
+}
+
+export default App;
